3a395007dca265e03587bd033430cf82f2f27d23
Aironys Garrido <aironys1@hotmail.com>
2022-03-24 15:29:57 -0300

    Metodos de Strings 2

---
+++ script.js
@@ -0,0 +1,18 @@
+let nome = "Somos os melhores ";
+
+let resutado = nome.slice(0,18); // Para pegar a posição inicial até o final.
+
+
+let resutado2 = resutado.slice(2,10)  // Pegando da posição 2 / 10
+let resutado3 = resutado.slice(5)  // Começando da posição 0
+
+console.log(resutado);
+console.log(resutado2);
+console.log(resutado3);
+
+
+let resultado4 = nome.substring(10,15); // Faz a mesma função do slice. Selecionando a posição inicial e a quantidade de caracteres que vc quer pegar.
+console.log(resultado4);
+
+
+
